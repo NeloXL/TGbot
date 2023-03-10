0a6bf1835b7d69c64ee3b93ed48777bd14300954
user <user@LEONID>
2023-03-11 03:00:40 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApp2/Start.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Telegram.Bot.Exceptions;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types;
+using Telegram.Bot;
+using Newtonsoft.Json.Linq;
+using Telegram.Bot.Types.ReplyMarkups;
+
+namespace ConsoleApp2
+{
+    internal class Start
+    {
+
+        public static async void StartMessage()
+        {
+            var botClient = new TelegramBotClient(Program._token);
+
+            using CancellationTokenSource cts = new();
+
+            ReceiverOptions receiverOptions = new()
+            {
+                AllowedUpdates = Array.Empty<UpdateType>()
+            };
+            botClient.StartReceiving(
+                updateHandler: HandleUpdateAsync,
+                pollingErrorHandler: HandlePollingErrorAsync,
+                receiverOptions: receiverOptions,
+                cancellationToken: cts.Token
+            );
+            var me = await botClient.GetMeAsync();
+
+            Console.ReadLine();
+
+            // Send cancellation request to stop bot
+            cts.Cancel();
+
+
+            async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+            {
+
+            }
+
+
+            Task HandlePollingErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+            {
+                var ErrorMessage = exception switch
+                {
+                    ApiRequestException apiRequestException
+                        => $"Telegram API Error:\n[{apiRequestException.ErrorCode}]\n{apiRequestException.Message}",
+                    _ => exception.ToString()
+                };
+
+                Console.WriteLine(ErrorMessage);
+                return Task.CompletedTask;
+            }
+        }
+    }
+}
