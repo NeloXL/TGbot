a5ff4424cac1a74d98db3336b31bed3e7b5d9b6d
Легенда <leonid.baklanov2015@gmail.com>
2023-03-14 14:09:27 +0500

    привет

---
+++ TGbot/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot;
+
+namespace TGbot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
